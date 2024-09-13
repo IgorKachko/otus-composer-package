57b53e03c0eec6f170402f2563e0c2e8756258a5
IgorKachko <igor.a.kachko@gmail.com>
2024-09-13 18:39:29 +0300

    add checkParenthesis method

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Процессор строк
 
 * getLength(string $str) - Узнать длину строки
+* checkParenthesis(string $str) - Проверка корректности открытых и закрытых скобок в строке
 
 ## Установка
 
@@ -12,5 +13,13 @@ composer require igor-kachko/otus-composer-package
 <?php  
   
 $processor = new StringProcessor();  
-echo $processor->getLength('my string')' // 9
+echo $processor->getLength('my string'); // 9
+
+try {
+    $processor->checkParenthesis("(1+2))(text())))()()()");
+    echo "Все хорошо";
+} catch(\Exception $e) (
+    echo $e->getMessage();
+)
+
 ```
--- src/StringProcessor.php
+++ src/StringProcessor.php
@@ -4,9 +4,32 @@ declare(strict_types=1);
 
 namespace IgorKachko\OtusComposerPackage;
 
+use Exception;
+
 class StringProcessor
 {
     public function getLength(string $s): int {
         return mb_strlen($s);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function checkParenthesis(string $str): bool {
+        $counter = 0;
+        for ($i=0; $i < strlen($str); $i++) {
+            $item = substr($str, $i, 1);
+            if($item === "(")  {
+                $counter++;
+            } elseif($item === ")") {
+                $counter--;
+            }
+
+            if($counter < 0) {
+                throw new Exception("Ошибка в порядке закрытия скобок на символе " . $i+1);
+            }
+        }
+
+        return true;
     }
 }
